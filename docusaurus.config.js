f3259c718af64994f65d4f04451622003a0dc9f0
wangqianwen0418 <wangqianwen0418@163.com>
2021-04-12 09:11:23 -0400

    disable dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ module.exports = {
   themeConfig: {
     colorMode: {
       defaultMode: 'light',
-      disableSwitch: false,
+      disableSwitch: true,
       respectPrefersColorScheme: true,
       switchConfig: {
         darkIcon: '🌙',
