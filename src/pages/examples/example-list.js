9928af8c34ed8eb5e7aa13f7b0a29c4f89f4129f
wangqianwen0418 <wangqianwen0418@163.com>
2021-04-20 18:50:08 -0400

    add examples

---
+++ src/pages/examples/example-list.js
@@ -0,0 +1,58 @@
+
+const basicExamples = [
+    {
+      title: 'Bar Chart',
+      imageUrl: 'https://raw.githubusercontent.com/gosling-lang/gosling-docs/master/images/bar_example.png',
+      url:'https://gosling-lang.github.io/gosling.js/?gist=wangqianwen0418/3acca936d08e676c9a274de73d094492',
+      description: (''),
+    },
+    {
+      title: 'Line Chart',
+      imageUrl: 'https://raw.githubusercontent.com/gosling-lang/gosling-docs/master/images/line_example.png',
+      url: 'https://gosling-lang.github.io/gosling.js/?gist=wangqianwen0418/12e9bc738ed19d325a900ff50d1f85dc',
+      description: (''),
+    },
+    {
+      title: 'Point Plot',
+      imageUrl: 'https://raw.githubusercontent.com/gosling-lang/gosling-docs/master/images/point_example.png',
+      url: 'https://gosling-lang.github.io/gosling.js/?gist=wangqianwen0418/e7ed2a61336b0ecc40211c8c3004388a',
+      description: (''),
+    },
+    {
+      title: 'Ideograms',
+      imageUrl: 'https://raw.githubusercontent.com/gosling-lang/gosling-docs/master/images/rect_example.png',
+      url: 'https://gosling-lang.github.io/gosling.js/?gist=wangqianwen0418/a5399812193a29fe7c85c519869a42ca',
+      description: (''),
+    },
+    {
+      title: 'Area Chart',
+      imageUrl: 'https://raw.githubusercontent.com/gosling-lang/gosling-docs/master/images/area_example.png',
+      url: 'https://gosling-lang.github.io/gosling.js/?gist=wangqianwen0418/213dcc25c61427bef243baefd8c36801',
+      description: (''),
+    },
+    {
+      title: 'Text Marks',
+      imageUrl: 'https://raw.githubusercontent.com/gosling-lang/gosling-docs/master/images/text_example.png',
+      url: 'https://gosling-lang.github.io/gosling.js/?gist=wangqianwen0418/3a3c7fce17876e6ef879d1aa499a4664',
+      description: (''),
+    },
+  ];
+  
+  const interactionExamples = [
+    {
+        title: 'Overview + Details',
+        imageUrl: 'https://raw.githubusercontent.com/gosling-lang/gosling-docs/master/images/tutorial/tutorial_multi_views.gif',
+        url: '',
+        description: ''
+    },
+    {
+        title: 'Semantic Zoom',
+        imageUrl: '',
+        url: '',
+        description: ''
+    }
+  ];
+  
+  const gallery = []
+
+  export {basicExamples, interactionExamples, gallery}
--- src/pages/examples/index.js
+++ src/pages/examples/index.js
@@ -1,36 +1,20 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
+import {basicExamples, interactionExamples, gallery} from './example-list.js';
 
-const features = [
-  {
-    title: 'Example',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (''),
-  },
-  {
-    title: 'Example',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (''),
-  },
-  {
-    title: 'Example',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (''),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
+function ExampleImage({imageUrl, title, url, description}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={clsx('col col--4', styles.feature)}>
       {imgUrl && (
         <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
+          <a href={url} target="_blank">
+            <img className={styles.featureImage} src={imgUrl} alt={title} />
+          </a>
         </div>
       )}
       <h3>{title}</h3>
@@ -44,20 +28,53 @@ export default function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={siteConfig.title}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}>
         <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
+          <header className={styles.sectionHeader}>
+            <h1>Basic Marks</h1>
+          </header>
+        {basicExamples && basicExamples.length > 0 && (
+          <section className={styles.examples}>
             <div className="container">
               <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
+                {basicExamples.map((props, idx) => (
+                  <ExampleImage key={idx} {...props} />
                 ))}
               </div>
             </div>
           </section>
         )}
+
+          <header className={styles.sectionHeader}>
+            <h1>Interactions</h1>
+          </header>
+        {interactionExamples && interactionExamples.length > 0 && (
+          <section className={styles.examples}>
+            <div className="container">
+              <div className="row">
+                {interactionExamples.map((props, idx) => (
+                  <ExampleImage key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+
+          <header className={styles.sectionHeader}>
+            <h1>Gallery</h1>
+          </header>
+        {gallery && gallery.length > 0 && (
+          <section className={styles.examples}>
+            <div className="container">
+              <div className="row">
+                {gallery.map((props, idx) => (
+                  <ExampleImage key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+
       </main>
     </Layout>
   );
