2e5b1a5d557273dd6a7106d5fa333f98af7c6761
wangqianwen0418 <wangqianwen0418@163.com>
2021-07-08 21:14:29 -0400

    feat: select vis in theme

--- src/pages/themes/index.jsx
+++ src/pages/themes/index.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './styles.module.css';
 
 import { BrowserGosling } from '../../../GoslingEditor';
-import { SPEC_LINK, SPEC_CIRCULAR } from '../../../GoslingEditor/SPECS.js';
+import * as SPECs from '../../../GoslingEditor/SPECS.js';
 
 
 export default function Themes() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
   const { themes } = siteConfig.customFields.themes
+  const visList = ['SPEC_CIRCULAR', 'SPEC_LINK']
+  const [ vis, setVis] = useState('SPEC_CIRCULAR')
 
 
-  const selectPanel = <><label for="cars">Choose a Visualization: </label>
-  <select name="vis" id="vis">
+  const selectPanel = <><label htmlFor="cars">Choose a Visualization: </label>
+  <select name="vis" id="vis" onChange={ (event) => setVis(event.target.value)}>
     <option value="SPEC_CIRCULAR"> Circular</option>
     <option value="SPEC_LINK"> Linking </option>
   </select> </>
+
   return (
     <Layout
       title="Theme">
@@ -31,8 +34,10 @@ export default function Themes() {
           {selectPanel}
           <div className="row">
 
-            {themes.map((them, idx) => (
-              <BrowserGosling spec={SPEC_LINK} />
+            {themes.map((theme, idx) => (
+              <div key={theme.name}>
+              <BrowserGosling spec={SPECs[vis]} />
+              </div>
             ))}
           </div>
         </div>
