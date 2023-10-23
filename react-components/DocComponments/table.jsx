98fcc1280610f9a558f61f55c4a8b9eaea32e6b3
etowahadams <etowahadams@gmail.com>
2023-10-23 18:46:22 -0400

    feat: update table to show more granular types

--- react-components/DocComponments/table.jsx
+++ react-components/DocComponments/table.jsx
@@ -22,7 +22,7 @@ export const TableWrapper = (props) => {
  * @property {Object} GoslingSchema
  * @property {boolean} [includeDescription=false]
  * @property {string[]} [hideProperties=[]] The properties you want to hide from the objName type
- * @property {Object} [secondaryObject={name: '', interaction: 'union'}] If you want the table to show the union of two object properties, set the name of the secondary object and the interaction type to union. 
+ * @property {Object} [secondaryObject={name: '', interaction: 'intersect'}] If you want the table to show the intersection of two object properties, set the name of the secondary object and the interaction type to intersect. 
  * For the difference between two objects, set the interaction to anything else.
  */
 
@@ -31,7 +31,7 @@ export const TableWrapper = (props) => {
  * @param {TableProps} props 
  * @returns {React.Component} <div><table>...</table></div>
  */
-const PropertyTable = ({ objName, GoslingSchema, includeDescription = false, hideProperties=[], secondaryObject={name: '', interaction: 'union'}}) => {
+const PropertyTable = ({ objName, GoslingSchema, includeDescription = false, hideProperties=[], secondaryObject={name: '', interaction: 'intersect'}}) => {
 
   var allDef = GoslingSchema["definitions"]
   /**
@@ -64,9 +64,9 @@ const PropertyTable = ({ objName, GoslingSchema, includeDescription = false, hid
     let propertyList = allPropertyList.filter(propertyName => !hideProperties.includes(propertyName) && !propertyName.startsWith('_'));
 
     if (secondaryObject.name != '') {
-      const shouldUnion = secondaryObject.interaction == 'union'
-      const unionPropertyList = Object.keys(allDef[secondaryObject.name]['properties'])
-      propertyList = propertyList.filter(propertyName => shouldUnion ? unionPropertyList.includes(propertyName) : !unionPropertyList.includes(propertyName) )
+      const shouldIntersect = secondaryObject.interaction == 'intersect'
+      const intersectPropertyList = Object.keys(allDef[secondaryObject.name]['properties'])
+      propertyList = propertyList.filter(propertyName => shouldIntersect ? intersectPropertyList.includes(propertyName) : !intersectPropertyList.includes(propertyName) )
     }
 
     const propertyTableRows = propertyList.map(key => {
@@ -98,12 +98,9 @@ const PropertyTable = ({ objName, GoslingSchema, includeDescription = false, hid
           // notes.push(<ReactMarkdown key='desription' children={description.replace(/\n\n/g, '\n')} />)
         }
       }
-
-
-
-
+      const stylized = `\`${key}\``
       return <tr key={key}>
-        <td>{key}</td>
+        <td><ReactMarkdown children={stylized} /></td>
         <td className='type'> <ReactMarkdown children={pType} /> </td>
         <td><ReactMarkdown children={notes.join(' ')} /> </td>
       </tr>
@@ -188,7 +185,7 @@ const parsePType = (propertyInfo, notes, propertyName, GoslingSchema) => {
     /** if an array of type reference */
     else if (propertyInfo?.items?.$ref != undefined) {
       const objName = propertyInfo['items']['$ref'].replace('#/definitions/', '')
-      pType = `[${objName}[]](#${objName.toLowerCase()})`
+      pType = `[${objName}[]](reference#${objName.toLowerCase()})`
     }
   }
   /**
@@ -197,7 +194,11 @@ const parsePType = (propertyInfo, notes, propertyName, GoslingSchema) => {
   else if (pType === 'object') {
     /** if the number of properties is less than 3 or if the property do not have a type name */
     if (propertyInfo['properties'] && (Object.keys(propertyInfo['properties']).length < 3 || propertyName == '')) {
-      pType = 'object'
+      if (propertyName == '') {
+        pType = 'object'
+      } else {
+        pType = `[${propertyName}](reference#${propertyName.toLowerCase()})`
+      }
       const { objNotes, otherNotes } = obj2str(propertyInfo, GoslingSchema)
       notes.push(`Each object follows the format \`${objNotes}\` (${otherNotes})`)
 
@@ -206,7 +207,7 @@ const parsePType = (propertyInfo, notes, propertyName, GoslingSchema) => {
     else if (propertyInfo['additionalProperties']) {
       pType = `{[k: string]: ${parsePType(propertyInfo['additionalProperties'], [], '', GoslingSchema)['pType']}}`
     } else {
-      pType = `[${propertyName}](#${propertyName.toLowerCase()})`
+      pType = `[${propertyName}](reference#${propertyName.toLowerCase()})`
     }
   }
   else if (pType === 'string') {
@@ -220,7 +221,7 @@ const parsePType = (propertyInfo, notes, propertyName, GoslingSchema) => {
    * noType but an anyOf Array
    */
   else if (pType == undefined && propertyInfo['anyOf']) {
-    const uniqueValues = [...new Set(propertyInfo['anyOf'].map(p => parsePType(p, [], '', GoslingSchema)['pType']))]
+    const uniqueValues = [...new Set(propertyInfo['anyOf'].map(p => parsePType(p, [], '', GoslingSchema)['pType']))];
     pType = uniqueValues.join(' | ')
   }
   /**
